90610cf92107fd0296a8a94c609c126c51430325
ryanboris <ryan.boris@gmail.com>
2019-03-20 21:15:14 -0400

    completes users query

--- schema.py
+++ schema.py
@@ -1,14 +1,33 @@
 import graphene
 import json
 import uuid
-from datetime import datetime
+from datetime import datetime as dt
 
 
 class User(graphene.ObjectType):
     id = graphene.ID(default_value=str(uuid.uuid4()))
     username = graphene.String()
-    created_at = graphene.DateTime(default_value=datetime.now())
+    created_at = graphene.DateTime(default_value=dt.now())
     is_admin = graphene.Boolean()
+
+
+class Post(graphene.ObjectType):
+    title = graphene.String()
+    content = graphene.String()
+
+
+class Query(graphene.ObjectType):
+    users = graphene.List(User, limit=graphene.Int())
+
+    def resolve_users(self, info, limit):
+        return [
+            User(id="1", username="John",
+                 created_at=dt.now(), is_admin=True),
+            User(id="2", username="Jen", created_at=dt.now(),
+                 is_admin=False),
+            User(id="3", username="Jane",
+                 created_at=dt.now(), is_admin=False),
+        ][:limit]
 
 
 class CreateUser(graphene.Mutation):
@@ -22,48 +41,36 @@ class CreateUser(graphene.Mutation):
         return CreateUser(user=user)
 
 
-class Query(graphene.ObjectType):
-    users = graphene.List(User, limit=graphene.Int())
+class CreatePost(graphene.Mutation):
+    post = graphene.Field(Post)
 
-    def resolve_users(self, info, limit):
-        return [
-            User(id="1", username="John",
-                 created_at=datetime.now(), is_admin=True),
-            User(id="2", username="Jen", created_at=datetime.now(),
-                 is_admin=False),
-            User(id="3", username="Jane",
-                 created_at=datetime.now(), is_admin=False),
-        ][:limit]
+    class Arguments:
+        title = graphene.String()
+        content = graphene.String()
+
+    def mutate(self, info, title, content):
+        post = Post(title=title, content=content)
+        return CreatePost(post=post)
 
 
 class Mutation(graphene.ObjectType):
     create_user = CreateUser.Field()
+    create_post = CreatePost.Field()
 
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
 result = schema.execute(
     '''
-    mutation($username: String) {
-        createUser(username: $username) {
-            user {
+
+    query getUsersQuery ($limit: Int!) {
+        users(limit: $limit) {
                 id
                 username
                 createdAt
-            }
-        }
-    }
-
-    query($limit: Int) {
-        users(limit: $limit) {
-            user {
-                id 
-                username
-                createdAt
-            }
         }
     }
     ''',
-    variable_values={'username': 'Dave', 'limit': 1}
+    variable_values={'limit': 3}
 )
 
 print(json.dumps(dict(result.data.items()), indent=2))
